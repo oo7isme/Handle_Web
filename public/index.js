4ddf8b253856782a7d45831b03ae95c6dfc5dc52
oo7isme <92167854+oo7isme@users.noreply.github.com>
2022-11-22 14:44:15 +0100

    first commit
    Display and Firebase connected

---
+++ public/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbb4M26XfGAZfoWFNKsPzqqWOBnPsgA6A",
+  authDomain: "handle-online.firebaseapp.com",
+  projectId: "handle-online",
+  storageBucket: "handle-online.appspot.com",
+  messagingSenderId: "404168210899",
+  appId: "1:404168210899:web:fb80a6d3a67912db1bd726"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
