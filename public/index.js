504a5d7184997384ddb42f7e7de8cebaf3b5e0b7
oo7isme <92167854+oo7isme@users.noreply.github.com>
2022-12-01 12:43:02 +0100

    modal

--- public/index.js
+++ public/index.js
@@ -23,4 +23,24 @@ function openModal() {
 document.getElementById("no").addEventListener("click", closeModal);
 function closeModal() {
   document.getElementById("popup").style.display = "none";
+  document.getElementById("text").value = "";
+}
+
+document.getElementById("yes").addEventListener("click", getPenger);
+function getPenger() {
+  const penger = document.getElementById("text").value;
+  document.getElementById("penger").innerHTML = penger + " " + "kr";
+  document.getElementById("popup").style.display = "none";
+  document.getElementById("text").value = "";
+}
+
+document.getElementById("add").addEventListener("click", openModalAdd);
+
+function openModalAdd() {
+  document.getElementById("popup-add").style.display = "flex";
+}
+document.getElementById("no").addEventListener("click", closeModalAdd);
+function closeModalAdd() {
+  document.getElementById("popup-add").style.display = "none";
+  document.getElementById("text").value = "";
 }
