f361f124216d28c9c1428216167bbb3b09969658
oo7isme <92167854+oo7isme@users.noreply.github.com>
2022-12-02 14:18:19 +0100

    add list and math

--- public/index.js
+++ public/index.js
@@ -29,7 +29,7 @@ function closeModal() {
 document.getElementById("yes").addEventListener("click", getPenger);
 function getPenger() {
   const penger = document.getElementById("text").value;
-  document.getElementById("penger").innerHTML = penger + " " + "kr";
+  document.getElementById("penger").innerHTML = penger;
   document.getElementById("popup").style.display = "none";
   document.getElementById("text").value = "";
 }
@@ -39,8 +39,60 @@ document.getElementById("add").addEventListener("click", openModalAdd);
 function openModalAdd() {
   document.getElementById("popup-add").style.display = "flex";
 }
-document.getElementById("no").addEventListener("click", closeModalAdd);
+document.getElementById("noProduct").addEventListener("click", closeModalAdd);
 function closeModalAdd() {
   document.getElementById("popup-add").style.display = "none";
-  document.getElementById("text").value = "";
+  document.getElementById("textProduct").value = "";
 }
+
+document.getElementById("yesProduct").addEventListener("click", addList)
+function addList(){
+  const itemDiv = document.createElement("div");
+  itemDiv.className = "itemDiv";
+  const itemText = document.createElement("p");
+  itemText.textContent = document.getElementById("textProduct").value;
+  itemText.className = "itemText";
+  itemDiv.appendChild(itemText);
+  document.getElementById("listContainer").appendChild(itemDiv);
+  document.getElementById("popup-add").style.display = "none";
+  document.getElementById("textProduct").value = "";
+}
+
+const container = document.querySelector('#listContainer');
+container.addEventListener('click', (event) => {
+  if(event.target.className == 'itemDiv')
+  { 
+    event.target.addEventListener("click", openModalMoney);
+
+    function openModalMoney(){} {
+      document.getElementById("popup-money").style.display = "flex";
+    }
+    document.getElementById("noPrice").addEventListener("click", closeModalMonney);
+    function closeModalMonney() {
+      document.getElementById("popup-money").style.display = "none";
+      document.getElementById("textPrice").value = "";
+    }
+    document.getElementById("yesPrice").addEventListener("click", getPris);
+  function getPris() {
+  const currentPenger = document.getElementById("penger").innerHTML;
+  const pris = document.getElementById("textPrice").value;
+  const container = document.querySelector('#listContainer');
+
+  document.getElementById("penger").innerHTML = currentPenger-pris ;
+  document.getElementById("popup-money").style.display = "none";
+  
+}
+}
+});
+
+// delete
+// const container = document.querySelector('#listContainer');
+// container.addEventListener('click', (event) => {
+//   if(event.target.className == 'itemDiv')
+//   { 
+//     event.target.remove();
+// }else {
+//   return;
+// }
+// });
+
